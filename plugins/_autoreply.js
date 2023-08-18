0293dfda683881b3ec02ccd6d616cd04b60a6c62
Rakrohaku9534 <Rakrohaku9534@github.com>
2023-08-18 21:37:13 +0700

    main

---
+++ plugins/_autoreply.js
@@ -0,0 +1,91 @@
+import fs from 'fs'
+import moment from 'moment-timezone'
+
+let handler = m => m
+
+handler.all = async function (m) {
+    if (m.chat.endsWith('status@broadcast')) {
+        console.log('sw cok')
+    }
+    let { isBanned } = db.data.chats[m.chat]
+    let { banned } = db.data.users[m.sender]
+    let { group } = db.data.settings[this.user.jid]
+    let setting = db.data.settings[this.user.jid]
+    let user = global.db.data.users[m.sender]
+    let name = await this.getName(m.sender)
+    
+const slm = `
+_wa'alaikumussalam wr.wb._
+
+${botdate}
+`.trim()
+    // salam
+    let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
+    let isSalam = reg.exec(m.text)
+    if (isSalam && !m.fromMe) {
+    await this.reply(m.chat, slm, m)
+}
+    	
+    // ketika ada yang invite/kirim link grup di chat pribadi
+    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
+        this.sendButton(m.chat, `┌「 *Undang Bot ke Grup* 」
+├ 30 Hari / Rp 2,000
+├ 30 Hari / Rp 3,000
+└────
+`.trim(), wm, 'Pemilik Bot', '.owner', m)
+    }
+const b = `Hai Kak ${name} 👋,\nSaya ${namabot} Apa Ada Yang Bisa Saya Bantu?\nKetik *#menu* Untuk Melihat Lihat Fitur Yang Di Sediakan Oleh Bot\nJangan Lupa Berdonasi 😁`
+    if (/^bot$/i.test(m.text)) {
+        await this.sendButton(m.chat, !(m.isGroup || m.isPrems) && group ? 'hanya grup' : isBanned ? 'chat banned' : banned ? 'user banned' : `${b}`, wm, !(m.isGroup || m.isPrems) && group ? 'donasi' : isBanned ? 'unban' : banned ? 'minta owner kalo mau di unban' : 'donasi', !(m.isGroup || m.isPrems) && group ? '.donasi' : isBanned ? '.unban' : banned ? '.owner' : '.donasi', m)
+    }
+
+
+    // backup db
+    if (setting.backup) {
+        if (new Date() * 1 - setting.backupDB > 1000 * 60 * 60) {
+            let d = new Date
+            let date = d.toLocaleDateString('id', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            })
+            await global.db.write()
+            this.reply(global.owner[0] + '@s.whatsapp.net', `Database: ${date}`, null)
+            let data = fs.readFileSync('./database.json')
+            await this.sendMessage(owner[0] + '@s.whatsapp.net', { document: data, mimetype: 'application/json', fileName: 'database.json' }, { quoted: null })
+            setting.backupDB = new Date() * 1
+        }
+    }
+
+    return !0
+}
+
+export default handler
+function ucapan() {
+    const time = moment.tz('Asia/Jakarta').format('HH')
+    res = "Selamat dinihari"
+    if (time >= 4) {
+        res = "Selamat pagi"
+    }
+    if (time > 10) {
+        res = "Selamat siang"
+    }
+    if (time >= 15) {
+        res = "Selamat sore"
+    }
+    if (time >= 18) {
+        res = "Selamat malam"
+    }
+    return res
+}
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
